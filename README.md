2566470579e2c34653cbc113a4d7fb2cd94e226c
Anton Niklasson <niklasson.anton@gmail.com>
2017-11-17 23:26:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# react-clickcopy
+
+Copy a piece of text by clicking on it.
+
+## Install
+
+```
+npm install --save react-clickcopy
+yarn add react-clickcopy
+```
+
+## Usage
+
+```javascript
+export default () => (
+  <h3><ClickCopy>http://localhost:8080</CopyClick><h3>
+);
+```
---
+++ index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ClickCopy extends React.Component {
+  constructor(props) {
+    super(props);
+
+    ['onClick', 'ref', 'copyText'].forEach((k) => { this[k] = this[k].bind(this); });
+  }
+
+  onClick() {
+    if (this.node) {
+      this.copyText();
+    }
+  }
+
+  copyText() {
+    const selection = window.getSelection();
+    const range = document.createRange();
+
+    range.selectNodeContents(this.node);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    document.execCommand('copy');
+    selection.removeAllRanges();
+  }
+
+  ref(node) {
+    this.node = node;
+  }
+
+  render() {
+    const { children, className } = this.props;
+    return (
+      <span ref={this.ref} onClick={this.onClick} className={className} style={{ position: 'relative' }}>
+        {children}
+      </span>
+    );
+  }
+}
+
+ClickCopy.defaultProps = {
+  className: '',
+};
+
+ClickCopy.propTypes = {
+  children: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+export default ClickCopy;
