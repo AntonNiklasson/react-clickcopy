6450b2230deb3b14bc532ac97fd0338a4b0dbbe5
Anton Niklasson <niklasson.anton@gmail.com>
2017-12-08 22:20:36 +0100

    feat: renaming the entry file

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  input: 'lib/index.js',
+  input: 'lib/ClickCopy.js',
   output: {
     file: 'dist/react-clickcopy.js',
     format: 'umd',
