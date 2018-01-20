d53b26ba486ba68fe42b7a9e306250f6e73e6913
Anton Niklasson <niklasson.anton@gmail.com>
2018-01-20 22:34:08 +0100

    tests are passing.

--- jestSetup.js
+++ jestSetup.js
@@ -1,4 +1,7 @@
+// React 16 needs rAF
+global.requestAnimationFrame = () => {};
+
+// Enzume needs some kind of adapter for React 16
 const enzyme = require("enzyme");
 const Adapter = require("enzyme-adapter-react-16");
-
 enzyme.configure({ adapter: new Adapter() });
